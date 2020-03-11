8330460988c4ef8335ec8bfb9c7f0ba485a8716c
Jhonatan Vinicius Paulino de Morais <jhonatan.morais@epl.gov.br>
2020-03-11 10:45:43 -0300

    first

---
+++ resources/js/views/Start.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>Start View</div>
+</template>
+
+<script>
+export default {
+    name: "Start"
+};
+</script>
+
+<style></style>
---
+++ webpack.mix.js
@@ -0,0 +1,30 @@
+const mix = require("laravel-mix");
+
+/* mix.options({
+    hmrOptions: {
+        host: "localhost",
+        port: "8080"
+    }
+});
+
+mix.webpackConfig({
+    devServer: {
+        port: "8080"
+    }
+}); */
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js("resources/js/app.js", "public/js").sass(
+    "resources/sass/app.scss",
+    "public/css"
+);
