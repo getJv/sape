987db58dafa0e4ec5d393117e306d216e083ea6a
Jhonatan Morais <jhonatanvinicius@gmail.com>
2020-03-26 03:30:03 +0000

    vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,14 @@
 import Vue from "vue";
 import router from "./router";
 import App from "./components/App";
+import stores from "./store";
 
 require("./bootstrap");
 
 const app = new Vue({
     el: "#app",
     router,
+    store,
     components: {
         App
     }
