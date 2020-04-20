3950fc71a1958675e069361cee1db064640576bc
Jhonatan Vinicius Paulino de Morais <jhonatan.morais@epl.gov.br>
2020-04-20 13:55:29 -0300

    card de fases layout

--- resources/js/components/ProjectStepper.vue
+++ resources/js/components/ProjectStepper.vue
@@ -1,49 +1,43 @@
 <template>
-    <v-card outlined>
-        <v-card-title>Fases do Projeto</v-card-title>
-        <v-divider></v-divider>
-        <v-card-text>
-            <p class="text-center" v-if="!projectWorkflow">
-                Este projeto ainda não possui um workflow. Crie um no menu
-                Gerenciar de workflow.
-            </p>
-            <v-stepper flat v-else v-model="e13" vertical>
-                <span
-                    v-for="(phase, index) in projectWorkflow.data"
-                    :key="phase.data.attributes.order"
-                >
-                    <v-stepper-step
-                        :step="phase.data.attributes.order"
-                        @click="e13 = phase.data.attributes.order"
-                    >
-                        <span>
-                            {{ phase.links.old_status.data.attributes.name }}
-                            <BtnAddEvent
-                                :owner_id="phase.links.old_status.data.id"
-                                :project_workflow_id="phase.data.id"
-                                :step_id="phase.data.attributes.order"
-                            />
-                        </span>
-                    </v-stepper-step>
-                    <v-stepper-content
-                        class="mb-3"
-                        :step="phase.data.attributes.order"
-                    >
-                        <v-card color="grey lighten-1">
-                            <v-card-text>
-                                <div v-if="index == 0">
-                                    <StepCard
-                                        v-for="event in eventFinder(
+  <v-card outlined>
+    <v-card-title>Fases do Projeto</v-card-title>
+    <v-divider></v-divider>
+    <v-card-text>
+      <p class="text-center" v-if="!projectWorkflow">
+        Este projeto ainda não possui um workflow. Crie um no menu
+        Gerenciar de workflow.
+      </p>
+      <v-stepper flat v-else v-model="e13" vertical>
+        <span v-for="(phase, index) in projectWorkflow.data" :key="phase.data.attributes.order">
+          <v-stepper-step
+            :step="phase.data.attributes.order"
+            @click="e13 = phase.data.attributes.order"
+          >
+            <span>
+              {{ phase.links.old_status.data.attributes.name }}
+              <BtnAddEvent
+                :owner_id="phase.links.old_status.data.id"
+                :project_workflow_id="phase.data.id"
+                :step_id="phase.data.attributes.order"
+              />
+            </span>
+          </v-stepper-step>
+          <v-stepper-content class="mb-3" :step="phase.data.attributes.order">
+            <v-card color="grey lighten-1">
+              <v-card-text>
+                <div v-if="index == 0">
+                  <StepCard
+                    v-for="event in eventFinder(
                                             phase.links.events,
                                             phase.data.attributes.old_status_id
                                         )"
-                                        :key="event.data.id"
-                                        :item="event"
-                                    />
-                                </div>
-                                <div v-else>
-                                    <StepCard
-                                        v-for="event in [
+                    :key="event.data.id"
+                    :item="event"
+                  />
+                </div>
+                <div v-else>
+                  <StepCard
+                    v-for="event in [
                                             ...eventFinder(
                                                 projectWorkflow.data[index - 1]
                                                     .links.events,
@@ -56,42 +50,49 @@
                                                     .old_status_id
                                             )
                                         ]"
-                                        :key="event.data.id"
-                                        :item="event"
-                                    />
-                                </div>
-                            </v-card-text>
-                        </v-card>
-                    </v-stepper-content>
-                    <v-divisor></v-divisor>
-                </span>
+                    :key="event.data.id"
+                    :item="event"
+                  />
+                </div>
+              </v-card-text>
+            </v-card>
+          </v-stepper-content>
+          <v-divisor></v-divisor>
+        </span>
 
-                <!-- Ultimo Step do pw -->
+        <!-- Ultimo Step do pw -->
 
-                <v-stepper-step
-                    v-if="projectWorkflow"
-                    :step="projectWorkflow.workflow_steps + 1"
-                    @click="e13 = projectWorkflow.workflow_steps + 1"
-                >
-                    <span>
-                        {{ last_step.links.new_status.data.attributes.name }}
-                        <BtnAddEvent
-                            :owner_id="last_step.links.new_status.data.id"
-                            :project_workflow_id="last_step.data.id"
-                            :step_id="projectWorkflow.workflow_steps + 1"
-                        />
-                    </span>
-                </v-stepper-step>
-                <v-stepper-content :step="projectWorkflow.workflow_steps + 1">
-                    <StepCard
-                        v-for="event in last_step.links.events.data"
-                        :key="event.data.id"
-                        :item="event"
-                    />
-                </v-stepper-content>
-            </v-stepper>
-        </v-card-text>
-    </v-card>
+        <v-stepper-step
+          v-if="projectWorkflow"
+          :step="projectWorkflow.workflow_steps + 1"
+          @click="e13 = projectWorkflow.workflow_steps + 1"
+        >
+          <span>
+            {{ last_step.links.new_status.data.attributes.name }}
+            <BtnAddEvent
+              :owner_id="last_step.links.new_status.data.id"
+              :project_workflow_id="last_step.data.id"
+              :step_id="projectWorkflow.workflow_steps + 1"
+            />
+          </span>
+        </v-stepper-step>
+        <v-stepper-content :step="projectWorkflow.workflow_steps + 1">
+          <v-card color="grey lighten-1">
+            <v-card-text>
+              <StepCard
+                v-for="event in eventFinder(
+                                            last_step.links.events,
+                                            last_step.data.attributes.new_status_id
+                                        )"
+                :key="event.data.id"
+                :item="event"
+              />
+            </v-card-text>
+          </v-card>
+        </v-stepper-content>
+      </v-stepper>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
@@ -101,37 +102,35 @@ import { mapGetters } from "vuex";
 
 /**TODO: transformar o tooltip em componente dinamico */
 export default {
-    name: "ProjecStepper",
-    components: {
-        StepCard,
-        BtnAddEvent
-    },
+  name: "ProjecStepper",
+  components: {
+    StepCard,
+    BtnAddEvent
+  },
 
-    computed: {
-        ...mapGetters(["projectWorkflow"]),
+  computed: {
+    ...mapGetters(["projectWorkflow"]),
 
-        last_step() {
-            return this.projectWorkflow.data[
-                this.projectWorkflow.workflow_steps - 1
-            ];
-        }
-    },
-    methods: {
-        eventFinder(eventCollection, stepId) {
-            //return phase.links.events.data.filter(
-            return eventCollection.data.filter(
-                event =>
-                    event.data.attributes.owner_id ==
-                    //phase.data.attributes.old_status_id
-                    stepId
-            );
-        }
-    },
-    data() {
-        return {
-            e13: 1
-        };
+    last_step() {
+      return this.projectWorkflow.data[this.projectWorkflow.workflow_steps - 1];
     }
+  },
+  methods: {
+    eventFinder(eventCollection, stepId) {
+      //return phase.links.events.data.filter(
+      return eventCollection.data.filter(
+        event =>
+          event.data.attributes.owner_id ==
+          //phase.data.attributes.old_status_id
+          stepId
+      );
+    }
+  },
+  data() {
+    return {
+      e13: 1
+    };
+  }
 };
 </script>
 
